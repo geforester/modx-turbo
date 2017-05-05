04c1a0dd71348b0233920e56e89fd11d3d8ff4b1
Roman Ilin <geforester@yandex.ru>
2017-05-05 23:31:25 +0300

    first commit

---
+++ packages.php
@@ -0,0 +1,213 @@
+<?php
+/*
+Скрипт надо запускать от юзера - владельца сайта, чтобы созданные файлы пакетов не принадлежали юзеру root
+$ sudo -u USERNAME php /var/www/USERNAME/packages.php /var/www/USERNAME/www/
+
+Или от root, а после выставить владельца:
+$ php /root/scripts/modx/packages.php /var/www/USERNAME/www/ && /var/www/USERNAME/chmod
+
+Чтобы запустить из веба, просто положите скрипт в корень или куда-нибудь глубже и вызовите по HTTP
+*/
+
+// >> Список репозиториев с компонентами для установки
+$providers = array(
+    array(
+        'name'		=> 'modx.com',
+        'service_url'	=> 'http://rest.modx.com/extras/',
+        'username'	=> '',
+        'api_key'	=> '',
+        'packages'	=> array(
+            'formit',
+            'MIGX',
+            'TinyMCE Rich Text Editor',
+            'simpleUpdater'
+        ),
+    ),
+    array(
+        'name'		=> 'Modstore',
+        'service_url'	=> 'http://modstore.pro/extras/',
+        'username'	=> '54@5444.ru',
+        'api_key'	=> '8be3f5a32253a4c32a65a7337347b236',
+        'packages'	=> array(
+            'ace',
+            'autoRedirector',
+            'pdotools',
+            'AjaxForm',
+            'minifyx',
+            'yTranslit',
+            'FastUploadTV'
+        ),
+    ),
+);
+// << Список репозиториев с компонентами для установки
+
+
+$countInstalled = 0;
+
+$console = !empty($argv) ? true : false; // Узнаём из консоли запустили или из веба
+
+if( $console )
+{
+    if( !empty($argv[1]) && file_exists( $argv[1] .'index.php' ) )
+    {
+        $current_dir = $argv[1];
+    }
+}
+
+
+// >> Подключаем
+define('MODX_API_MODE', true);
+
+$current_dir = !empty($current_dir) ? $current_dir : dirname(__FILE__) .'/';
+$index_php = $current_dir .'index.php';
+
+$i=0;
+while( !file_exists( $index_php ) && $i < 9 )
+{
+    $current_dir = dirname(dirname($index_php)) .'/';
+    $index_php = $current_dir .'index.php';
+    $i++;
+}
+
+if( file_exists($index_php) )
+{
+    require_once $index_php;
+}
+
+if( !is_object($modx) )
+{
+    _print('ERROR: Не удалось подгрузить MODX');
+    die;
+}
+// << Подключаем
+
+
+// >> Включаем обработку ошибок
+$modx->getService('error','error.modError');
+$modx->setLogLevel(modX::LOG_LEVEL_ERROR);
+$modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
+// << Включаем обработку ошибок
+
+
+$modx->switchContext('mgr');
+$modx->addPackage('modx.transport', MODX_CORE_PATH .'model/');
+
+$modx->getVersionData();
+$productVersion = $modx->version['code_name'] .'-'. $modx->version['full_version'];
+
+foreach( $providers as $prov )
+{
+    if( !$provider = $modx->getObject('transport.modTransportProvider', array('service_url' => $prov['service_url']) ) )
+    {
+        $provider = $modx->newObject('transport.modTransportProvider');
+
+        $provider->fromArray($prov);
+        $provider->save();
+    }
+
+    $provider->getClient();
+
+    foreach( $prov['packages'] as $packageName )
+    {
+        $response = $provider->request('package', 'GET',
+            array(
+                'query' => $packageName
+            ));
+
+        if( !empty($response) )
+        {
+            $foundPackages = simplexml_load_string($response->response);
+
+            if( $foundPackages['total'] > 0 )
+            {
+                foreach( $foundPackages as $foundPackage )
+                {
+
+                    if( strtolower((string)$foundPackage->name) == strtolower($packageName) )
+                    {
+                        if( !$modx->getCount('transport.modTransportPackage', array('signature' => (string)$foundPackage->signature)) )
+                        {
+                            $sig = explode('-', $foundPackage->signature);
+                            $versionSignature = explode('.', $sig[1]);
+
+                            file_put_contents( MODX_CORE_PATH .'packages/'. $foundPackage->signature .'.transport.zip', file_get_contents($foundPackage->location) );
+
+                            $package = $modx->newObject('transport.modTransportPackage');
+
+                            $package->set('signature', $foundPackage->signature);
+
+                            $package->fromArray(
+                                array(
+                                    'created'	=> date('Y-m-d h:i:s'),
+                                    'updated'	=> null,
+                                    'state'		=> 1,
+                                    'workspace'	=> 1,
+                                    'provider'	=> $provider->id,
+                                    'source'	=> $foundPackage->signature .'.transport.zip',
+                                    'package_name'	=> (string)$foundPackage->name,
+                                    'version_major'	=> $versionSignature[0],
+                                    'version_minor'	=> !empty($versionSignature[1]) ? $versionSignature[1] : 0,
+                                    'version_patch'	=> !empty($versionSignature[2]) ? $versionSignature[2] : 0,
+                                ));
+
+                            if( !empty($sig[2]) )
+                            {
+                                $r = preg_split('/([0-9]+)/', $sig[2], -1, PREG_SPLIT_DELIM_CAPTURE);
+
+                                if( is_array($r) && !empty($r) )
+                                {
+                                    $package->set('release', $r[0]);
+                                    $package->set('release_index', (isset($r[1]) ? $r[1] : '0'));
+                                }
+                                else {
+                                    $package->set('release', $sig[2]);
+                                }
+
+                                if( $success = $package->save() )
+                                {
+                                    $package->install();
+                                    _print('Installed: "'. (string)$foundPackage->name .'"');
+                                    $countInstalled++;
+                                }
+                                else {
+                                    _print('Could not save: "'. (string)$foundPackage->name .'"');
+                                }
+                            }
+                        }
+                        else {
+                            _print('Already exists: "'. (string)$foundPackage->name .'"');
+                        }
+                    }
+                }
+            }
+            else {
+                _print('Not found: "'. $packageName .'"');
+            }
+        }
+    }
+}
+
+if( $countInstalled > 0 )
+{
+    _print('Done!', 0);
+}
+
+
+/* >> Функция принта для консоли и веба */
+function _print( $str='', $br=true )
+{
+    global $console;
+
+    if( empty($str) ) {return;}
+
+    if( $console )
+    {
+        fwrite(STDOUT, $str . ($br ? "\n" : ""));
+    }
+    else {
+        print $str . ($br ? "<br />" : "");
+    }
+}
+/* << */
+
+exit;
---
+++ settings.php
@@ -0,0 +1,195 @@
+<?php
+require_once dirname(__FILE__).'/config.core.php';
+include_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+
+$modx= new modX();
+$modx->initialize('mgr');
+$modx->setLogLevel(modX::LOG_LEVEL_INFO);
+$modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
+header("Content-type: text/plain");
+
+
+
+// *****************    Settings
+echo "<br> Preparing Settings …";
+$settings = array(
+	  'cultureKey' => 'ru'
+	, 'fe_editor_lang' => 'ru'
+	, 'publish_default' => 1
+	, 'tvs_below_content' => 1
+	, 'upload_maxsize' => '10485760'
+	, 'manager_lang_attribute' => 'ru'
+	, 'manager_language' => 'ru'
+    , 'error_page' => 2
+    , 'cache_disabled' => 1
+    , 'cache_resource' => 0
+
+    //mail
+    , 'mail_smtp_auth' => 1
+    , 'mail_smtp_hosts' => 'smtp.yandex.ru'
+    , 'mail_smtp_port' => 465
+    , 'mail_smtp_prefix' => 'ssl'
+    , 'mail_use_smtp' => 1
+
+
+	//url
+	, 'automatic_alias' => 1
+	, 'friendly_urls' => 1
+	, 'container_suffix' => '.html'
+	, 'friendly_alias_translit' => 'russian'
+	, 'site_status' => 0
+    , 'use_alias_path' => 1
+
+
+);
+foreach ($settings as $k => $v) {
+	$opt = $modx->getObject('modSystemSetting', array('key' => $k));
+	if (!empty($opt)){
+		$opt->set('value', $v);
+		$opt->save();
+      	echo 'edited '.$k.' = '.$v."\n";
+    } else {
+    	$newOpt = $modx->newObject('modSystemSetting');
+    	$newOpt->set('key', $k);
+    	$newOpt->set('value', $v);
+    	$newOpt->save();
+    	echo 'added '.$k.' = '.$v."\n";
+    }
+}
+echo "Done!";
+
+
+
+
+// *****************    Resources
+echo "<br> Creating resources …";
+$resources = array(
+	  array('pagetitle' => 'Страница не найдена',
+			'template' => 1,
+			'published' => 1,
+			'hidemenu' => 1,
+			'alias' => '404',
+			'content_type' => 1,
+			'richtext' => 1,
+			'content' => '
+<div style="width: 500px; margin: -30px auto 0; overflow: hidden;padding-top: 25px;">
+	<div style="float: left; width: 100px; margin-right: 50px; font-size: 75px;margin-top: 45px;">404</div>
+	<div style="float: left; width: 350px; padding-top: 30px; font-size: 14px;">
+		<h2>Страница не найдена</h2>
+		<p style="margin: 8px 0 0;">Страница, на которую вы зашли, вероятно, была удалена с сайта, либо ее здесь никогда не было.</p>
+		<p style="margin: 8px 0 0;">Возможно, вы ошиблись при наборе адреса или перешли по неверной ссылке.</p>
+		<h3 style="margin: 15px 0 0;">Что делать?</h3>
+		<ul style="margin: 5px 0 0 15px;">
+			<li>проверьте правильность написания адреса,</li>
+			<li>перейдите на <a href="[[!++site_url]]">главную страницу</a> сайта,</li>
+			<li>или <a href="javascript:history.go(-1);">вернитесь на предыдущую страницу</a>.</li>
+		</ul>
+	</div>
+</div>
+			'
+			)
+	, array('pagetitle' => 'sitemap',
+	  		'template' => 0,
+	  		'published' => 1,
+	  		'hidemenu' => 1,
+	  		'alias' => 'sitemap',
+            'content_type' => 2,
+	  		'richtext' => 0,
+	  		'content' =>'[[!pdoSitemap]]'
+	  		)
+	, array('pagetitle' => 'robots',
+			'template' => 0,
+			'published' => 1,
+			'hidemenu' => 1,
+			'alias' => 'robots',
+			'content_type' => 3,
+			'richtext' => 0,
+			'content' => 'User-agent: * Disallow: /manager/ Disallow: /assets/components/ Allow: /assets/uploads/ Disallow: /core/ Disallow: /connectors/ Disallow: /index.php Disallow: /search Disallow: /profile/ Disallow: *? Host: [[++site_url]] Sitemap: [[++site_url]]sitemap.xml'
+			)
+);
+foreach ($resources as $attr) {
+	// $modx->error->reset();
+	$response = $modx->runProcessor('resource/create', $attr);
+}
+echo "Done!";
+
+// todo Category
+
+
+
+// chunks
+echo "<br> Creating Chunks …";
+$chunks = array(
+    array('name' => 'childs',
+        'description' => '',
+        'snippet' => '<div class="childs">[[pdoMenu? &parents=`[[*id]]` &level=`1`]]</div>'
+    )
+, array('name' => 'HEAD',
+        'description' => '',
+        'snippet' => '',
+        'static' => '1',
+        'static_file' => '/assets/chunk.html'
+    )
+
+);
+foreach ($chunks as $attr) {
+    // $modx->error->reset();
+    $response = $modx->runProcessor('element/chunk/create', $attr);
+    echo "created {$item}";
+}
+echo "Done!";
+
+
+
+
+// *****************    todo TVs
+
+echo "<br> Creating TVs …";
+// $modx->error->reset();
+$crttv = $modx->runProcessor('element/tv/create', array(
+    	'name' => 'img',
+    	'caption' => 'Изображение',
+    	'type' => 'checkbox',
+    	'els' => 'Да==@CHUNK childs'
+));
+echo "Done!";
+
+
+
+
+// *****************    Prepare filesystem
+echo "<br> Preparing filesystem …";
+$folders = array(
+    'assets/templates',
+    'assets/templates/chunks',
+    'assets/templates/css',
+    'assets/templates/fonts',
+    'assets/templates/img',
+    'assets/templates/js',
+    'assets/images'
+);
+
+foreach ($folders as $item) {
+    mkdir($item, 0755);
+}
+
+$files = array(
+    'assets/templates/chunks/head.html',
+    'assets/templates/chunks/header.html',
+    'assets/templates/chunks/footer.html',
+    'assets/templates/chunks/modal.html',
+    'assets/templates/chunks/slider.row.html',
+    'assets/templates/chunks/news.row.html',
+    'assets/templates/chunks/item.row.html',
+);
+
+foreach ($files as $item) {
+    file_put_contents($item,'', FILE_APPEND);
+    echo "created {$item}";
+}
+
+echo "All right! Let's go!";
+
+// $modx->cacheManager->refresh();
+$modx->runProcessor('system/clearcache');
+
